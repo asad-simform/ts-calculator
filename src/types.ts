96702391296e8a254370cdaedf421969c758fcd6
Asad Merchant <asad.merchant@simformsolutions.com>
2026-03-31 14:31:57 +0530

    add types

---
+++ src/types.ts
@@ -0,0 +1,22 @@
+export type strOrNum = string | number
+
+export type priorityType = -1 | 1 | 2 | 3 | 4
+
+export type scientificArr = ["sin", "cos", "tan", "log", "ln", "sqrt"]
+
+export type scientificOp = scientificArr[number]
+
+export type Operator = "+" | "-" | "/" | "*" | "%" | "^" | "!" | "minus" | "(" | ")"
+
+export interface IStack {
+    resiprocal(str: string): number,
+    tokenGenerator(str: string): strOrNum[]
+    postfix(arr: strOrNum[]): strOrNum[]
+    evaluatePostfix(arr: strOrNum[]): [number]
+    toggleSign(arr: strOrNum[]): strOrNum[]
+}
+
+export interface IHistory {
+    appendHistory(exp: string, val: number): void 
+    loadLocal(): void 
+}
